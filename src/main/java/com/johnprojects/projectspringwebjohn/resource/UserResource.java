280a36f5c2b9087f409698bab7a583b74c8ac0e7
Jonathan Sampaio <120474111+JohnSampa@users.noreply.github.com>
2025-11-17 11:48:27 -0300

    User entity and resourse

---
+++ src/main/java/com/johnprojects/projectspringwebjohn/resource/UserResource.java
@@ -0,0 +1,24 @@
+package com.johnprojects.projectspringwebjohn.resource;
+
+import com.johnprojects.projectspringwebjohn.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(
+                1,
+                "Jonathan",
+                "e@email.com",
+                "8888222",
+                "123123"
+        );
+        return ResponseEntity.ok().body(u);
+    }
+}
