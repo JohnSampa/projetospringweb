06398aabc056976d235645fb64e03cd11e05ff3c
Jonathan Sampaio <120474111+JohnSampa@users.noreply.github.com>
2025-11-17 10:08:18 -0300

    Project created

---
+++ src/main/java/com/johnprojects/projectspringwebjohn/ProjectspringwebjohnApplication.java
@@ -0,0 +1,13 @@
+package com.johnprojects.projectspringwebjohn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectspringwebjohnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectspringwebjohnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johnprojects/projectspringwebjohn/ProjectspringwebjohnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnprojects.projectspringwebjohn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectspringwebjohnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
