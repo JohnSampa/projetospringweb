3c9b2241d2279e5e54f1f6073bda1357b46e6591
Jonathan Sampaio <aurijona192@gmail.com>
2025-12-01 02:24:02 -0300

    Exception handler findById

--- src/main/java/com/johnprojects/projectspringwebjohn/services/UserService.java
+++ src/main/java/com/johnprojects/projectspringwebjohn/services/UserService.java
@@ -2,6 +2,7 @@ package com.johnprojects.projectspringwebjohn.services;
 
 import com.johnprojects.projectspringwebjohn.entities.User;
 import com.johnprojects.projectspringwebjohn.repositories.UserRepository;
+import com.johnprojects.projectspringwebjohn.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
     public User findById(Long id){
         Optional<User> user = userRepository.findById(id);
 
-        return user.get();
+        return user.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
     public User insert(User user){
